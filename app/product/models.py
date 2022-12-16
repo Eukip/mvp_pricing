a83b0ba453d52d39f175179ba2dc6f93eae42e63
Eukip <wtff6a@gmail.com>
2022-12-16 23:09:04 +0600

    almost done models

--- app/product/models.py
+++ app/product/models.py
@@ -9,30 +9,49 @@ User = get_user_model()
 class Category(models.Model):
     title = models.CharField(max_length=300)
 
+    def __str__(self) -> str:
+        return self.title
 
 class SubCategory(models.Model):
     title = models.CharField(max_length=300)
     related_category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.title + ' '+ str(self.related_category.title)
 
 
 class SubSubCategory(models.Model):
     title = models.CharField(max_length=300)
     related_sub_categoy = models.ForeignKey(SubCategory, on_delete=models.CASCADE, blank=True, null=True)
 
+    def __str__(self) -> str:
+        return self.title + ' ' + str(self.related_sub_categoy.title)
+
 
 class RoleProduct(models.Model):
     title = models.CharField(max_length=300)
 
+    def __str__(self) -> str:
+        return self.title
+
 
 class Product(models.Model):
-    full_title = models.CharField(max_length=300)
+    thing = models.CharField(max_length=300)
     vendor_code = models.CharField(max_length=300, blank=True, null=True)
-    article = models.CharField(max_length=300, blank=True, null=True)
-    # url_product
+    article_provider = models.CharField(max_length=300, blank=True, null=True)
+    nomenclature_1c_code = models.IntegerField(blank=True, null=True)
+    last_barcode = models.IntegerField(blank=True, null=True)
+    url_product = models.URLField(blank=True, null=True)
     # todo: наследование и ссылки конкурента как-то отделить
     purchase_price = models.PositiveIntegerField(blank=True, null=True)
-    price_before_discount = models.PositiveIntegerField(blank=True, null=True)
+    current_price_before_discount = models.PositiveIntegerField(blank=True, null=True)
+    new_price_before_discount = models.PositiveIntegerField(blank=True, null=True)
     price_after_dicount = models.PositiveIntegerField(blank=True, null=True)
+    current_discount = models.PositiveIntegerField(blank=True, null=True)
+    recommended_discount = models.PositiveIntegerField(blank=True, null=True)
+    agreed_discount = models.PositiveIntegerField(blank=True, null=True)
+    cuurent_discount_promo = models.PositiveIntegerField(blank=True, null=True)
+    new_discount_promo = models.PositiveIntegerField(blank=True, null=True)
     promotional_price = models.PositiveIntegerField(blank=True, null=True)
     standart_price = models.PositiveIntegerField(blank=True, null=True)
     max_price = models.PositiveIntegerField(blank=True, null=True)
@@ -43,7 +62,13 @@ class Product(models.Model):
     create_date = models.DateField(auto_now_add=True)
     role = models.ForeignKey(RoleProduct, on_delete=models.CASCADE, blank=True, null=True)
     creator = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    # id_strategy = models.ForeignKey(Strategy, on_delete=models.CASCADE, blank=True, null=True)
-    # competitors = models.ForeignKey()
-    # остаток склад регион
-    # аналог товаров ссылки
+    remainder = models.PositiveIntegerField(blank=True, null=True)
+    storage = models.CharField(max_length=300, blank=True, null=True)
+    region = models.CharField(max_length=300, blank=True, null=True)
+
+    @property
+    def full_title(self):
+        return self.thing + ' ' + self.title + ' ' + self.brand
+
+    def __str__(self) -> str:
+        return self.full_title

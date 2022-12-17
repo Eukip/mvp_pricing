b65228851236e4094fdf407382507cbf685d90d4
praktikant <ek27global312@gmail.com>
2022-12-17 10:31:12 +0600

    Populating db and excel: Upd

--- app/product/utils.py
+++ app/product/utils.py
@@ -1,8 +1,8 @@
-# from app.config.settings import BASE_DIR
-
-import pandas as pd
-
 import os
+import pandas as pd
+from itertools import islice
+
+from app.product.models import Product
 
 
 def parse_excel():
@@ -32,15 +32,6 @@ def parse_excel():
     return result
 
 
-def populate_products_excel(article_provider, new_price_before_discount,
-                            agreed_discount):
-    df = pd.read_excel(os.getcwd() + "/app/parsing/7eb50656ea82adcb.xlsx")
-    row_to_update = df["Артикул поставщика"] == str(article_provider)
-    df.loc[row_to_update, "Новая розн. цена (до скидки)"] = new_price_before_discount
-    df.loc[row_to_update, "Согласованная скидка, %"] = agreed_discount
-    df.to_excel("output.xlsx")
-
-
 def populate_excel(data):
     df = pd.read_excel(os.getcwd() + "/app/parsing/7eb50656ea82adcb.xlsx")
     for item in data:
@@ -52,16 +43,25 @@ def populate_excel(data):
     df.to_excel("output.xlsx")
 
 
-var = [{
-    '7st_351144_030864-WN79/1': {
-        'new_price': 100,
-        'agreed_discount': 10
-    }
-},
-    {
-        'buy2_N1ME0144/1': {
-            'new_price': 100,
-            'agreed_discount': 10
-        }
-    }
-]
+def populate_products_db():
+    data = parse_excel()
+    data_keys = data.keys()
+    batch_size = len(data_keys)
+    objs = (Product(thing=data[key]["Бренд"],
+                    vendor_code=data[key]["Предмет"],
+                    article_provide=data[key]["Артикул поставщика"],
+                    nomenclature_lc_code=data[key]["Номенклатура (код 1С)"],
+                    last_barcode=data[key]["Последний баркод"],
+                    remainder=data[key]["Остаток товара (шт.)"],
+                    current_price_before_discount=data[key]["Текущая розн. цена (до скидки)"],
+                    new_price_before_discount=data[key]["Новая розн. цена (до скидки)"],
+                    current_discount=data[key]["Текущая скидка на сайте, %"],
+                    recommended_discount=data[key]["Рекомендованная скидка, %"],
+                    agreed_discount=data[key]["Согласованная скидка, %"],
+                    cuurent_discount_promo=data[key]["Текущая скидка по промокоду, %"],
+                    new_discount_promo=data[key]["Новая скидка по промокоду, %"])
+            for key in data_keys)
+    batch = list(islice(objs, batch_size))
+    if not batch:
+        print("No batch created")
+    Product.objects.bulk_create(batch, batch_size)

658c89a1069b64bc96f1ccb432501b15a092c900
Eukip <wtff6a@gmail.com>
2022-12-19 00:28:30 +0600

    started search func for product by article

--- app/strategy/utils.py
+++ app/strategy/utils.py
@@ -6,3 +6,19 @@
 
 # как варинт для монго подключить вторую бд 
 # написать модельки используя djongo
+import requests
+from product.models import Product
+
+def seacrh_product_by_article(article):
+    product_response = requests.get(
+        url=f"http://95.217.21.252:8001/products/{article}/"
+    )
+    if product_response.status_code != 200:
+        return "Продукт не найден"
+    
+    product = Product.objects.create(
+        thing=product_response.json()['article']['article']['card']['imt_name'],
+        
+    )
+
+    

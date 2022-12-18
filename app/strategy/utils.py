456a402005c16b6beada182deaca4b36f5f9add5
Eukip <wtff6a@gmail.com>
2022-12-18 21:58:23 +0600

    json example structure for strategy is done

---
+++ app/strategy/utils.py
@@ -0,0 +1,10 @@
+from services import StrategyLogicOperator
+
+operator = StrategyLogicOperator()
+
+
+def parse_strategy(strategy):
+    operation = None
+    # получили логический оператор по ходу парсинга:
+    if operation == "EQUAL":
+        
